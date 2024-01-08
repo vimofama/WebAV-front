9dd932565ffef183460f9927237afd822e86f37d
Mateo Morales <vimofama@outlook.com>
2024-01-08 18:29:44 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      height: {
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        176: "44rem",
+        192: "48rem",
+        208: "52rem",
+      },
+      width: {
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        176: "44rem",
+        192: "48rem",
+        208: "52rem",
+      },
+    },
+  },
+  plugins: [],
+};
